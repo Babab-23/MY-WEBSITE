0a6cc691dd5b2e08bf800f3123325e1b7e5d4873
Babab-23 <adrianidialu@gmail.com>
2024-11-19 21:13:36 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example of addig interactive functionality
+document.querySelectorAll("button").forEach(button => {
+    button.addEventListener("click",() => {
+        alert("Item added to cart!");
+    });
+});
